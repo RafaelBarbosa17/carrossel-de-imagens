7831ba4f7f2ec5366831455878b2166d5fdedbcf
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2023-01-16 09:04:19 -0300

    reforma do código para se adaptar a responsividade

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# carrossel-de-Images
+# Carrossel de Revisões
+
 Simples carrossel de imagens com HTML, CSS e JavaScript.
 
-Utilizei a API de pokemons para buscar dados a serem renderizados na página.
+Utilizei a API de pokemons para buscar dados e renderiza-los na página.
 
 [Link para vizualização da página](https://rafaelbarbosa17.github.io/carrossel-de-imagens/)
--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-const boxs = document.querySelectorAll('.box');
+
+// variáveis que contém os elementos html
+const boxs = document.querySelectorAll('.poke-box');
 const n = document.getElementById('n');
 const p = document.getElementById('p');
 const container = document.querySelector('.container');
 
+// index para representar o elemento a ser ultilizado
 let index = 0;
-let scX = 0;
-let widthBox;
-boxs[index].style.minWidth = '40%'
-boxs[index].style.minHeight = '400px'
 
+// função que altera o index e aplica 'active' ao elemento que estiver na posição correta
 const changeIndex = () => {
-    boxs[index].style.minWidth = '40%';
-    boxs[index].style.minHeight = '400px';
-    boxs.forEach(i => {
-        if (i.className !== `box ${index}`) {
-            i.style.minWidth = '30%';
-            i.style.minHeight = '300px'
-        }
-    })
-    container.scroll(scX, 0)
-    // console.log(index)
+    for (let box of boxs) {
+        box.classList.remove('active')
+    }
+    boxs[index].classList.add('active');
 }
 
-const changeSizeBox = () => {
-    boxs.forEach(box => {
-        widthBox = box.clientWidth + 4;
-        console.log(widthBox)
-    })
-}
+changeIndex()
 
+// ouvintes de evento para realizar o scroll da tela manualmente e corretamente de acordo com a necessidade
 n.addEventListener('click', () => {
-    changeSizeBox()
     if (index < boxs.length - 1) {
-        index += 1;
-        scX += widthBox
+        index++;
+        container.scroll((container.clientWidth / 100) * 25 * index, 0);
+        changeIndex()
     }
-    changeIndex()
 })
-
 p.addEventListener('click', () => {
-    changeSizeBox()
     if (index > 0) {
-        index -= 1;
-        scX -= widthBox
+        index--;
+        container.scroll((container.clientWidth / 100) * 25 * index, 0);
+        changeIndex()    
     }
-    changeIndex()
 })
 
-const items = document.querySelectorAll('.box .item');
-
+// função assincrona que utiliza a api de pokemons e gera elementos html de acordo com os dados retornados
 const getPokemons = async (p) => {
+    // url representa o endereço da api para chamada
     const url = `https://pokeapi.co/api/v2/pokemon/${p}`
     const response = await fetch(url)
+    // jsonResponse representa os dados da resposta em formato json
     const jsonResponse = await response.json();
+    // data é um objeto contendo todos os elementos que serão necessarios para construção das imagens
     const data = {
         nome: await jsonResponse.name,
         imgSrc: await jsonResponse.sprites.other.dream_world.front_default,
     }
     // console.log(data)
-    items[p - 1].innerHTML = `<h1 class="poke-name"> ${data.nome} </h1> <img class="poke-img" src="${data.imgSrc}">`
+    // box recebe como conteúdo uma string que será transformada em elemento html
+    boxs[p - 1].innerHTML = `
+        <div class="poke-item">
+            <h1 class="poke-name"> ${data.nome} </h1>
+            <img class="poke-img" src="${data.imgSrc}">
+        </div>
+        `
 }
 
-for (let i = 0; i < items.length; i++) {
+// um loop que chama a função de gerar os elementos de acordo com o número de boxs que ouver na página
+for (let i = 0; i < boxs.length; i++) {
     const parameter = i + 1;
     getPokemons(parameter);
 }
